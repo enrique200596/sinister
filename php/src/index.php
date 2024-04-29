7b1efc8378b42ad35e5dd355584dd8dc85b62b8f
Luis Enrique Illescas Morales <enrique200596@gmail.com>
2024-04-29 13:35:59 +0000

    Mejoras de component

--- php/src/index.php
+++ php/src/index.php
@@ -2,9 +2,12 @@
 require_once 'component.php';
 $formulario = new Component('form', ['method' => 'POST', 'action' => '#']);
 $sectionEmail = new Component('section', ['id' => 'sectionEmail']);
-$labelEmail = new Component('label', ['id' => 'labelEmail', 'for' => 'inputEmail']);
+$labelEmail = new Component('label', ['id' => 'labelEmail', 'for' => 'inputEmail'],'Correo electrónico');
 $inputEmail = new Component('input', ['id' => 'inputEmail', 'type' => 'email', 'name' => 'email']);
-$spanEmail = new Component('span', ['id' => 'spanEmail']);
+$spanEmail = new Component('span', ['id' => 'spanEmail'], 'Error: correo no registrado.');
 $sectionEmail->addSubComponent($labelEmail->getAttribute('id'), $labelEmail);
 $sectionEmail->addSubComponent($inputEmail->getAttribute('id'), $inputEmail);
 $sectionEmail->addSubComponent($spanEmail->getAttribute('id'), $spanEmail);
+$formulario->addSubComponent($sectionEmail->getAttribute('id'), $sectionEmail);
+$formulario->build();
+echo $formulario->getHtml();
